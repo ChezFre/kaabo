2ef4231a339ac82d84b69cf0ef67955db8cfe404
Frederic Vandeborne <fre@novation.be>
2017-10-24 20:49:26 +0200

    Added logging statements

--- routes/index.js
+++ routes/index.js
@@ -78,6 +78,8 @@ router.get('/webhook', (req, res) => {
 function handleMessage(sender_psid, received_message) {
     let response;
 
+    console.log('handleMessage', received_message.text);
+
     // Check if the message contains text
     if (received_message.text) {    
         // Create the payload for a basic text message
