2aa6a32cce002d175d4d39d7e9203bc8102f536b
Frederic Vandeborne <fre@novation.be>
2017-10-25 22:16:49 +0200

    Test with greeting

--- routes/index.js
+++ routes/index.js
@@ -141,7 +141,7 @@ function handlePostback(sender_psid, received_postback) {
 
     if(payload === "REGISTER_USER") {
         response = {
-            "text": "Je gegevens werden opgeslagen. Vanaf nu krijg je een Facebook melding als iemand zich aanmeldt aan de balie";
+            "text": "Je gegevens werden opgeslagen. Vanaf nu krijg je een Facebook melding als iemand zich aanmeldt aan de balie"
         }
     }
     
