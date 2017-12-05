7a27db9a6dc0e7a07efb2acb8be1c8a3f6a02f5c
Frederic Vandeborne <fre@novation.be>
2017-12-05 07:14:38 +0100

    Test with message types

--- routes/index.js
+++ routes/index.js
@@ -67,7 +67,7 @@ router.post('/notify', (req, res, next) => {
     // Call Button when phone nr is entered?
     callSendAPI("1367522643370788", response); // Even fixed psid toevoegen van mezelf, daarna via contentful psid ophalen per gebruiker
 
-    res.sendStatus(200).send({ description: 'Message sent' })
+    res.sendStatus(200);//.send({ description: 'Message sent' })
 
 });
 
